0c9873829362dd7bd710cd7f0dac4c2030946f59
jo <jo@surikat.pro>
2017-12-21 16:50:30 +0100

    bugfix isWin32AbsolutePath

--- src/isWin32AbsolutePath.js
+++ src/isWin32AbsolutePath.js
@@ -1,12 +1,4 @@
-// from https://github.com/sindresorhus/path-is-absolute
-
-// const splitDeviceRe = /^([a-zA-Z]:|[\\/]{2}[^\\/]+[\\/]+[^\\/]+)?([\\/])?([\s\S]*?)$/
-const splitDeviceRe = /^(([a-zA-Z]){1}:|[\\/]{2}[^\\/]+[\\/]+[^\\/]+)?([\\/])?([\s\S]*?)$/
+const regex = /^[a-zA-Z]{1}:[\\/]|[\\\\]/
 export default function (path) {
-  const result = splitDeviceRe.exec(path)
-  const device = result[1] || ''
-  const isUnc = Boolean(device && device.charAt(1) !== ':')
-
-  // UNC paths are always absolute
-  return Boolean(result[2] || isUnc)
+  return regex.test(path)
 }
