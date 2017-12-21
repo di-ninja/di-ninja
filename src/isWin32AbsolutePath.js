3db82d24e630951b9ec739c0ffe35e5883b54287
jo <jo@surikat.pro>
2017-12-21 16:15:56 +0100

    clean

--- src/isWin32AbsolutePath.js
+++ src/isWin32AbsolutePath.js
@@ -1,11 +1,11 @@
-//from https://github.com/sindresorhus/path-is-absolute
+// from https://github.com/sindresorhus/path-is-absolute
 
-const splitDeviceRe = /^([a-zA-Z]:|[\\/]{2}[^\\/]+[\\/]+[^\\/]+)?([\\/])?([\s\S]*?)$/;
-export default function(path){
-  const result = splitDeviceRe.exec(path);
-  const device = result[1] || '';
-  const isUnc = Boolean(device && device.charAt(1) !== ':');
+const splitDeviceRe = /^([a-zA-Z]:|[\\/]{2}[^\\/]+[\\/]+[^\\/]+)?([\\/])?([\s\S]*?)$/
+export default function (path) {
+  const result = splitDeviceRe.exec(path)
+  const device = result[1] || ''
+  const isUnc = Boolean(device && device.charAt(1) !== ':')
 
   // UNC paths are always absolute
-  return Boolean(result[2] || isUnc);
+  return Boolean(result[2] || isUnc)
 }
