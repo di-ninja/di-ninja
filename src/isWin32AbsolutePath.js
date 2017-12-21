dc28b2b1285322967dc7f61fff5c9ee4d6397f1c
jo <jo@surikat.pro>
2017-12-21 16:36:48 +0100

    bugfix to one drive letter only

--- src/isWin32AbsolutePath.js
+++ src/isWin32AbsolutePath.js
@@ -1,6 +1,7 @@
 // from https://github.com/sindresorhus/path-is-absolute
 
-const splitDeviceRe = /^([a-zA-Z]:|[\\/]{2}[^\\/]+[\\/]+[^\\/]+)?([\\/])?([\s\S]*?)$/
+// const splitDeviceRe = /^([a-zA-Z]:|[\\/]{2}[^\\/]+[\\/]+[^\\/]+)?([\\/])?([\s\S]*?)$/
+const splitDeviceRe = /^(([a-zA-Z]){1}:|[\\/]{2}[^\\/]+[\\/]+[^\\/]+)?([\\/])?([\s\S]*?)$/
 export default function (path) {
   const result = splitDeviceRe.exec(path)
   const device = result[1] || ''
